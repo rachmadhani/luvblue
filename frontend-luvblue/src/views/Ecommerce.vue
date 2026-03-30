d350518aacea58cc8e8639d9920a231ad1962ab4
RachmadFidhani <simokart3@gmail.com>
2026-03-30 16:40:26 +0700

    fixing vercel 2

--- frontend-luvblue/src/views/Ecommerce.vue
+++ frontend-luvblue/src/views/Ecommerce.vue
@@ -27,8 +27,8 @@
 <script>
 import AdminLayout from '../components/layout/AdminLayout.vue'
 import EcommerceMetrics from '../components/ecommerce/EcommerceMetrics.vue'
-import MonthlyTarget from '../components/ecommerce/MonthlySale.vue'
-import MonthlySale from '../components/ecommerce/MonthlyTarget.vue'
+import MonthlyTarget from '../components/ecommerce/MonthlyTarget.vue'
+import MonthlySale from '../components/ecommerce/MonthlySale.vue'
 import CustomerDemographic from '../components/ecommerce/CustomerDemographic.vue'
 import StatisticsChart from '../components/ecommerce/StatisticsChart.vue'
 import RecentOrders from '../components/ecommerce/RecentOrders.vue'
